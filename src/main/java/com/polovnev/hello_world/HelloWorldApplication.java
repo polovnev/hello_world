2dad58328377927502cae2d230303c3ceed3e1fc
Oleksandr Polovnev <opolo@ciklum.com>
2020-09-20 16:52:01 +0300

    change message

--- src/main/java/com/polovnev/hello_world/HelloWorldApplication.java
+++ src/main/java/com/polovnev/hello_world/HelloWorldApplication.java
@@ -3,7 +3,7 @@ package com.polovnev.hello_world;
 public class HelloWorldApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		System.out.println("Hello, Sasha!");
 	}
 
 }
